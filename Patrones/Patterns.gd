7f0db07162671721d1194f2843ea568ea223bbcf
yaelatletl <yael.atletl@gmail.com>
2024-05-16 11:00:59 -0600

    Patterns full implementation

--- Patrones/Patterns.gd
+++ Patrones/Patterns.gd
@@ -1,8 +1,10 @@
-extends Node2D
+extends Control
 
 #List of any possible texture we can use, here go piggy, donkey, dog and cat
 var textures = [preload("res://assets/Round/pig.png"), preload("res://assets/round/duck.png"), preload("res://assets/round/dog.png"), preload("res://assets/round/cow.png"), preload("res://assets/PNG/BLUE/letter.png")]
+var duplicate_from = []
 
+signal selected(node)
 @onready var nodes = [
 	$CenterContainer/GridContainer/S2D,
 	$CenterContainer/GridContainer/S2D3,
@@ -20,14 +22,33 @@ func _enter_tree():
 	var tween = get_tree().create_tween()
 	tween.tween_property($CenterContainer, "scale", Vector2(0.5,0.5), 1).set_trans(Tween.TRANS_ELASTIC)
 
-func _exit_tree():
+func remove():
 	#Tween animation, from big to small, using ease in
 	var tween = get_tree().create_tween()
-	tween.tween_property($CenterContainer, "scale", Vector2(0,0), 1).set_trans(Tween.TRANS_BOUNCE)
+	tween.tween_property($CenterContainer, "scale", Vector2(0,0), 0.7).set_trans(Tween.TRANS_SPRING)
+	await tween.finished
+	#Remove the node from the scene tree
+	queue_free()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#Generate the pattern
-	for i in range(9):
-		nodes[i].texture = textures[randi() % textures.size()]
+	$CenterContainer/Button.pressed.connect(_on_Button_pressed)
+	if duplicate_from.size() == 0:
+		for i in range(9):
+			nodes[i].texture = textures[randi() % textures.size()]
+	else:
+		for i in range(9):
+			nodes[i].texture = duplicate_from[i].texture
 
+func _on_Button_pressed():
+	#Emit the signal to the parent
+	emit_signal("selected", self)
+
+func select():
+	#Select the node, visually
+	$CenterContainer/ColorRect.visible = true
+
+func deselect():
+	#Deselect the node, visually
+	$CenterContainer/ColorRect.visible = false
--- PatronesJuego.gd
+++ PatronesJuego.gd
@@ -1,12 +1,77 @@
 extends Control
 
-@onready var container = $HBoxContainer/VBoxContainer/GridContainer
+var selected = [] #Only two elements can be selected at a time
+var pairs = {}
+
+@onready var container : GridContainer= $HBoxContainer/VBoxContainer/GridContainer
+@export var pattern : PackedScene
+
+var mistakes = 0
+var score = 0
+var level = 2
+var timer = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	start_game()
+	populate_level(level)
+
+func start_game():
+	mistakes = 0
+	score = 0
+	level = 2
+	timer = get_tree().create_timer(120) #2 minutes to complete the game
+	timer.timeout.connect(end_game)
+
+func end_game():
+	timer.stop()
+	print("Game Over")
+	print("Score: ", score)
+	print("Mistakes: ", mistakes)
+	print("Level: ", level)
+	#Go back to the main menu
+
+func _on_pattern_selected(pattern_node):
+	if pattern_node in selected:
+		selected.erase(pattern_node)
+		pattern_node.deselect()
+	else:
+		selected.append(pattern_node)
+		pattern_node.select()
+	if selected.size() == 2:
+		if pairs[selected[0]] == selected[1]:
+			selected[0].remove()
+			selected[1].remove()
+			score += 1
+			pairs.erase(pairs[selected[0]])
+			pairs.erase(selected[0])
+			selected = []
+			if pairs.size() == 0:
+				level += 1
+				populate_level(level)
+		else:
+			selected[0].deselect()
+			selected[1].deselect()
+			mistakes += 1
+			selected = []
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func populate_level(difficulty):
+	print(ceil(difficulty/3.0))
+	for i in range(ceil(difficulty/3.0)):
+		var new_pattern = pattern.instantiate()
+		container.add_child(new_pattern)
+		var duplicated = new_pattern.duplicate()
+		duplicated.duplicate_from = new_pattern.nodes
+		container.add_child(duplicated)
+		duplicated.selected.connect(_on_pattern_selected )
+		new_pattern.selected.connect(_on_pattern_selected)
+		pairs[new_pattern] = duplicated
+		pairs[duplicated] = new_pattern
+	randomize_children()
+
+func randomize_children():
+	var children = container.get_children()
+	for i in range(children.size()):
+		var random_index = randi() % children.size()
+		container.move_child(children[i], random_index)
